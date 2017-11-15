befc31db1d9fbb844a08b7f4aa7654ac270ad5e8
jge162 <31228460+jge162@users.noreply.github.com>
2017-11-14 22:42:16 -0800

    Updated documentIndex.h 11/14/17 @ 10:42 pm

--- DocumentIndex.h
+++ DocumentIndex.h
@@ -1,77 +1,75 @@
 #ifndef	DocumentIndex_h
 #define	DocumentIndex_h
 
-//****************************************************************************************
-//
-//	INCLUDE FILES
-//
-//****************************************************************************************
 #include	<fstream>
 #include	<map>
 #include	<set>
 #include	<string>
+#include    <vector>
 
 using namespace std;
 
-//****************************************************************************************
-//
-//	CONSTANT DEFINITIONS
-//
-//****************************************************************************************
-
-//****************************************************************************************
-//
-//	CLASSES, TYPEDEFS AND STRUCTURES
-//
-//****************************************************************************************
 typedef	string::size_type	StringSize;
+typedef set<string> StringSet;
+typedef StringSet::iterator    StringSetIterator;
+typedef vector<int> NumberVector;
+
+struct WordData
+{
+    WordData():count_(0) {}
+    int count_;
+    NumberVector pageNumber_;
+};
+
+typedef map <string, WordData>      WordMap;
+typedef WordMap::iterator           WordMapIterator;
+typedef WordMap::value_type         WordMapValue;
+typedef pair<WordMapIterator, bool> WordMapStatus;
+
 
 class	DocumentFile
 {
-	public:
-		DocumentFile() : pageNumber_(1) { }
-		
-		void	Close();
-		
-		int		GetPageNumber();			//	Returns the current pge number.
-		
-		string	GetWord();					//	Returns the next legal word not on the exception list;
-											//	returns an empty string if there are no more words in
-											//	the line.
-		
-		bool	LoadExclusions(const string& name);	//	Loads a list of words to be excluded from the index
-													//	from a file of the given name.
-		
-		bool	Open(const string& name);	//	Opens a document file of the given name.
-		
-		bool	Read();						//	Reads the next line of the document file, skipping over
-											//	the double empty lines that mark page separations.
-											//	Returns false if there are no more lines in the file.
-		
-	private:
-		StringSize	beginPosition_;
-		
-		fstream		file_;
-		
-		int			pageNumber_;
-		
-		string		text_;
+public:
+    DocumentFile() : pageNumber_(1) { }
+    
+    void	Close();                            //  Closes current document
+    int		GetPageNumber();                    //	Returns the current pge number.
+    string	GetWord();                          //	Returns the next legal word not on the exception list;
+                                                //	returns an empty string if there are no more words in
+                                                //	the line.
+    bool	LoadExclusions(const string& name);	//	Loads a list of words to be excluded from the index
+                                                //	from a file of the given name.
+    bool	Open(const string& name);           //	Opens a document file of the given name.
+    bool	Read();                             //	Reads the next line of the document file, skipping over
+                                                //	the double empty lines that mark page separations.
+                                                //	Returns false if there are no more lines in the file.
+    
+    
+private:
+    
+    StringSize      beginPosition_;
+    StringSet       exclusion_;
+    
+    fstream         file_;
+    int             pageNumber_;
+    int             page_ = 0;
+    string          text_;
+    vector <string> line_;
+    
 };
 
 class	DocumentIndex
 {
-	public:
-		void	Create(DocumentFile& documentFile);	//	Creates an index for the given document file.
-
-		void	Show(ostream& stream);
-
-		void	Write(ostream& indexFile);	//	Writes the index to the given file.
-											//	The argument is a stream so that this function
-											//	can be called to wrtite its output to cout for
-											//	test purposes.
-	
-	private:
-
+public:
+    void	Create(DocumentFile& documentFile);	//	Creates an index for the given document file.
+    void	Show (ostream& stream);
+    void	Write(ostream& indexFile);	//	Writes the index to the given file.
+                                        //	The argument is a stream so that this function
+                                        //	can be called to wrtite its output to cout for
+                                        //	test purposes.
+private:
+    
 };
 
 #endif
+
