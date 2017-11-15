b3cab54e612744b29c765022adde867fd6cfed3c
jge162 <31228460+jge162@users.noreply.github.com>
2017-11-14 22:44:26 -0800

    Update GetLine.h

--- GetLine.h
+++ GetLine.h
@@ -1,1 +1,23 @@
-#ifndef GetLine_h#define GetLine_h//****************************************************************************************//	INCLUDE FILES#include	<iostream>#include	<string>using namespace std;//****************************************************************************************//	CONSTANT DEFINITIONS//****************************************************************************************//	CLASSES, TYPEDEFS AND STRUCTURES//****************************************************************************************//	FUNCTION PROTOTYPESbool	GetLine(istream& stream, string& text);bool	GetLine(istream& stream, string& text, const string& delimiter);#endif
+#ifndef GetLine_h
+#define GetLine_h
+//
+//****************************************************************************************
+//	INCLUDE FILES
+#include	<iostream>
+#include	<string>
+
+using namespace std;
+
+//****************************************************************************************
+//	CONSTANT DEFINITIONS
+
+//****************************************************************************************
+//	CLASSES, TYPEDEFS AND STRUCTURES
+
+//****************************************************************************************
+//	FUNCTION PROTOTYPES
+bool	GetLine(istream& stream, string& text);
+
+bool	GetLine(istream& stream, string& text, const string& delimiter);
+
+#endif
